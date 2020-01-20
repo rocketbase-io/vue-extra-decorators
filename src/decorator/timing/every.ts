1ab120f701826e2b9d7cadb807363ec0cdc776b2
Merlin Reichwald <merlin.reichwald@gmail.com>
2020-01-20 10:39:19 +0100

    fix: make every-interval decorator actually call its callback

--- src/decorator/timing/every.ts
+++ src/decorator/timing/every.ts
@@ -34,7 +34,7 @@ export function Every(ms: number = 1000) {
   return (target: any, key: string, desc: TypedPropertyDescriptor<AnyFunction>) => {
     const interval = Symbol(`interval:${key}`);
     return withSideEffects(
-      cxt => (cxt[interval] = setInterval(() => cxt[key].bind(cxt), ms)),
+      cxt => (cxt[interval] = setInterval(() => cxt[key].bind(cxt)(), ms)),
       cxt => clearInterval(cxt[interval])
     )(target, key);
   };

b2f5cc0c757c4272a13be95c83ad74afa9c20155
Merlin Reichwald <merlin.reichwald@rocketbase.io>
2020-02-27 11:28:58 +0100

    style: simplify statusOf

--- src/util/status-of.ts
+++ src/util/status-of.ts
@@ -1,7 +1,6 @@
 export function statusOf(obj: any): number | undefined {
-  if (!obj) return;
   if (typeof obj === "number") return obj;
-  if (typeof obj !== "object") return;
+  if (!obj || typeof obj !== "object") return;
   if ("response" in obj && typeof obj.response === "object") return statusOf(obj.response);
   if ("status" in obj && typeof obj.status === "number") return obj.status;
 }

7cb3f60e8438452d05bae404d8ba0632eb359dfb
Merlin Reichwald <merlin.reichwald@rocketbase.io>
2020-03-10 14:34:22 +0100

    chore: better docs generation

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,7 +2,6 @@ import sourcemaps from "rollup-plugin-sourcemaps";
 import commonjs from "rollup-plugin-commonjs";
 import ts from "@wessberg/rollup-plugin-ts";
 import paths from "rollup-plugin-ts-paths";
-import apiExtractor from "@rocketbase/rollup-plugin-api-extractor";
 import execute from "@rocketbase/rollup-plugin-exec";
 import sequential from "@rocketbase/rollup-plugin-sequential";
 import { name, globals, external } from "./package";
@@ -27,18 +26,8 @@ export default {
     commonjs(),
     sequential(
       [
-        apiExtractor({
-          config: "build/api-extractor.json",
-          override: { name },
-          cleanup: false
-        }),
         execute(
-          [
-            "api-documenter markdown --output-folder docs --input-folder dist",
-            "rimraf temp api-extractor.json dist/*.*.d.ts",
-            `bestzip '${name}.zip' dist/*`,
-            `bestzip '${name}-docs.zip' docs/*`
-          ],
+          ["typedoc", "rimraf temp dist/*.*.d.ts", `bestzip '${name}.zip' dist/*`, `bestzip '${name}-docs.zip' docs/*`],
           {
             stdio: "ignore"
           }

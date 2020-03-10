dc6ebd3c8bd04d178c1a0e53d6c11a35a519e4e9
Merlin Reichwald <merlin.reichwald@rocketbase.io>
2020-03-10 15:00:30 +0100

    chore: streamline build process even more

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,7 +2,6 @@ import sourcemaps from "rollup-plugin-sourcemaps";
 import commonjs from "rollup-plugin-commonjs";
 import ts from "@wessberg/rollup-plugin-ts";
 import paths from "rollup-plugin-ts-paths";
-import execute from "@rocketbase/rollup-plugin-exec";
 import { name, globals, external } from "./package";
 import banner from "./banner";
 
@@ -18,17 +17,5 @@ export default {
     banner
   })),
   external,
-  plugins: [
-    paths(),
-    ts({ tsconfig: "tsconfig.build.json" }),
-    sourcemaps(),
-    commonjs(),
-    execute(
-      ["typedoc", "rimraf temp dist/*.*.d.ts", `bestzip '${name}.zip' dist/*`, `bestzip '${name}-docs.zip' docs/*`],
-      {
-        stdio: "ignore",
-        once: true
-      }
-    )
-  ]
+  plugins: [paths(), ts({ tsconfig: "tsconfig.build.json" }), sourcemaps(), commonjs()]
 };

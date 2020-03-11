a3b75906cbac8c27cb2d451ec77cd547c130dc01
Merlin Reichwald <merlin.reichwald@rocketbase.io>
2020-03-11 10:38:02 +0100

    docs: add documentation for watch decorators

--- src/decorator/listener/watch.ts
+++ src/decorator/listener/watch.ts
@@ -6,8 +6,68 @@ interface WatchOptionsWithProp extends WatchOptions {
   prop: string;
 }
 
+/**
+ * Watch a property for changes
+ * @param property - The property to watch
+ * @remarks
+ * You can also pass an options object instead of just a property
+ * to set it to watch immediately or deeply.
+ * There are also flavors for these uses: {@link IWatch}, {@link DWatch}, {@link IDWatch}
+ * @example
+ * ```typescript
+ * @Data({ default: "" }) private value!: string;
+ * @Watch("value")
+ * private onValueChange(newValue: string, oldValue: string) {
+ *   // ...Implementation
+ * }
+ * ```
+ * {@link Watch}, {@link IWatch}, {@link DWatch}, {@link IDWatch}
+ * @public
+ */
 export function Watch(property: string): TypedPropertyDecorator<AnyFunction>;
+
+/**
+ * Watch a property for changes
+ * @param opts - The options for the watch, `prop` being the property to watch
+ * for, `immediate` being a flag if this watch should be executed immediately
+ * and `deep` being a flag if deep mutations to the watched property should
+ * trigger the watch
+ * @remarks
+ * You can also pass a string as a shorthand.
+ * There are also flavors for these uses: {@link IWatch}, {@link DWatch}, {@link IDWatch}
+ * @example
+ * ```typescript
+ * @Data({ default: "" }) private value!: string;
+ * @Watch({ prop: "value", immediate: true })
+ * private onValueChange(newValue: string, oldValue: string) {
+ *   // ...Implementation
+ * }
+ * ```
+ * {@link Watch}, {@link IWatch}, {@link DWatch}, {@link IDWatch}
+ * @public
+ */
 export function Watch(opts: WatchOptionsWithProp): TypedPropertyDecorator<AnyFunction>;
+
+/**
+ * Watch a property for changes
+ * @param opts - The property to watch or the options for the watch, `prop` being
+ * the property to watch for, `immediate` being a flag if this watch should be
+ * executed immediately and `deep` being a flag if deep mutations to the
+ * watched property should trigger the watch
+ * @remarks
+ * You can also pass a string as a shorthand.
+ * There are also flavors for these uses: {@link IWatch}, {@link DWatch}, {@link IDWatch}
+ * @example
+ * ```typescript
+ * @Data({ default: "" }) private value!: string;
+ * @Watch({ prop: "value", immediate: true })
+ * private onValueChange(newValue: string, oldValue: string) {
+ *   // ...Implementation
+ * }
+ * ```
+ * {@link Watch}, {@link IWatch}, {@link DWatch}, {@link IDWatch}
+ * @public
+ */
 export function Watch(opts: string | WatchOptionsWithProp): TypedPropertyDecorator<AnyFunction> {
   if (typeof opts === "string") opts = { prop: opts };
   const { prop } = opts;
@@ -20,14 +80,66 @@ export function Watch(opts: string | WatchOptionsWithProp): TypedPropertyDecorat
   });
 }
 
+/**
+ * Watch a property for changes, immediately
+ * @param property - The property to watch
+ * @remarks
+ * This decorator is synonymous to using {@link Watch} with `immediate` set to `true`.
+ * There are also flavors for these uses: {@link Watch}, {@link DWatch}, {@link IDWatch}
+ * @example
+ * ```typescript
+ * @Data({ default: "" }) private value!: string;
+ * @IWatch("value")
+ * private onValueChange(newValue: string, oldValue: string) {
+ *   // ...Implementation
+ * }
+ * ```
+ * {@link Watch}, {@link IWatch}, {@link DWatch}, {@link IDWatch}
+ * @public
+ */
 export function IWatch(property: string): TypedPropertyDecorator<AnyFunction> {
   return Watch({ prop: property, immediate: true });
 }
 
+/**
+ * Watch a property for changes, deeply
+ * @param property - The property to watch
+ * @remarks
+ * This decorator is synonymous to using {@link Watch} with `deep` set to `true`.
+ * There are also flavors for these uses: {@link Watch}, {@link IWatch}, {@link IDWatch}
+ * @example
+ * ```typescript
+ * @Data({ default: {} }) private value!: any;
+ * @DWatch("value")
+ * private onValueChange(newValue: any, oldValue: any) {
+ *   // ...Implementation
+ * }
+ * ```
+ * {@link Watch}, {@link IWatch}, {@link DWatch}, {@link IDWatch}
+ * @public
+ */
 export function DWatch(property: string): TypedPropertyDecorator<AnyFunction> {
   return Watch({ prop: property, deep: true });
 }
 
+/**
+ * Watch a property for changes, deeply and immediately
+ * @param property - The property to watch
+ * @remarks
+ * This decorator is synonymous to using {@link Watch} with `deep`
+ * and `immediate` set to `true`.
+ * There are also flavors for these uses: {@link Watch}, {@link IWatch}, {@link DWatch}
+ * @example
+ * ```typescript
+ * @Data({ default: {} }) private value!: any;
+ * @IDWatch("value")
+ * private onValueChange(newValue: any, oldValue: any) {
+ *   // ...Implementation
+ * }
+ * ```
+ * {@link Watch}, {@link IWatch}, {@link DWatch}, {@link IDWatch}
+ * @public
+ */
 export function IDWatch(property: string): TypedPropertyDecorator<AnyFunction> {
   return Watch({ prop: property, deep: true, immediate: true });
 }

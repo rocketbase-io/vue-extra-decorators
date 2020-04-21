2098db415b9df1880baae7ac3b1727a68d77a809
Merlin Reichwald <merlin.reichwald@gmail.com>
2020-04-21 05:46:15 +0200

    feat: make busystate decorator work outside of vue contexts

--- src/decorator/state/busy/busy-state.ts
+++ src/decorator/state/busy/busy-state.ts
@@ -1,6 +1,7 @@
 import { getBusyState, getBusyStateSubscribers } from "src/util/get-busy-state";
 import { TypedPropertyDecorator } from "src/types";
-import { Snowflake } from "src/util";
+import { getState, setState, Snowflake } from "src/util";
+import { isPrototype } from "src/util/is-prototype";
 import { optionsExtension } from "src/vue";
 
 /**
@@ -99,19 +100,12 @@ export function BusyState(idOrOpts?: string | { global?: true; id?: string }) {
   if (!idOrOpts) idOrOpts = "";
   if (typeof idOrOpts === "string") idOrOpts = { id: idOrOpts };
   const { id, global } = idOrOpts;
-  return optionsExtension(key => {
-    const subscriber = Symbol(`Busy State Subscriber: ${key}`);
-    return {
-      data(this: any) {
-        getBusyStateSubscribers(global ? undefined : this, id).push(
-          (this[subscriber] = (state: Snowflake[]) => (this[key] = !!state.length))
-        );
-        return () => ({ [key]: !!getBusyState(global ? undefined : this, id).length });
-      },
-      destroyed(this: any): void {
-        const subs = getBusyStateSubscribers(global ? undefined : this, id);
-        subs.splice(subs.indexOf(this[subscriber]), 1);
+  return (proto: any, key: string | symbol) => {
+    Object.defineProperty(proto, key, {
+      get(): boolean {
+        if (isPrototype(this)) throw new Error("Cannot access state on prototype");
+        return !!getBusyState(global ? undefined : this, id).length;
       }
-    };
-  });
+    });
+  };
 }

a8bddf664d602fbff361633f8bb5fe052c99061c
Merlin Reichwald <merlin.reichwald@gmail.com>
2020-06-18 15:13:03 +0200

    feat: enable side effects to be dom dependant

--- src/vue/with-side-effects.ts
+++ src/vue/with-side-effects.ts
@@ -6,19 +6,30 @@ import { createDecorator } from "vue-class-component";
  * Creates a vue decorator that requires setup and teardown
  * @param on - The setup of the decorator
  * @param off - The teardown of the decorator
+ * @param dom - Whether the side effects depend on the rendered dom elements
  * @internal
  */
 export function withSideEffects(
   on: (cxt: any, key: string) => any,
-  off: (cxt: any, key: string) => any
+  off: (cxt: any, key: string) => any,
+  dom?: boolean
 ): TypedPropertyDecorator<any> {
   return createDecorator((options, key) => {
-    options.created = append(options.created, function() {
+    const onHandler = function(this: any) {
       on.call(this, this, key);
-    });
-    options.destroyed = append(options.destroyed, function() {
+    };
+    const offHandler = function(this: any) {
       off.call(this, this, key);
-    });
+    };
+    if (dom) {
+      options.mounted = append(options.mounted, onHandler);
+      options.updated = append(options.updated, onHandler);
+      options.beforeUpdate = append(options.beforeUpdate, offHandler);
+      options.beforeDestroy = append(options.beforeDestroy, offHandler);
+    } else {
+      options.created = append(options.created, onHandler);
+      options.destroyed = append(options.destroyed, offHandler);
+    }
     return options;
   });
 }
